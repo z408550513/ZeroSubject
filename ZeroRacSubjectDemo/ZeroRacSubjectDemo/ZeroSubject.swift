1d85886cfb24a2f76b7cadd93e6ef5b762b4a556
z408550513 <z408550513@163.com>
2016-12-26 19:28:03 +0800

    Add files via upload

---
+++ ZeroRacSubjectDemo/ZeroRacSubjectDemo/ZeroSubject.swift
@@ -0,0 +1,23 @@
+//
+//  ZeroSubject.swift
+//  ZeroRacSubjectDemo
+//
+//  Created by ptsdesigner on 2016/12/26.
+//  Copyright © 2016年 Zero. All rights reserved.
+//
+
+import UIKit
+
+class ZeroSubject: NSObject {
+    var block : ((value : AnyObject?)->())?
+    func sendNext(value: AnyObject?) {
+    
+        if self.block != nil {
+            self.block!(value: value)
+        }
+    }
+    func subscribe(subscriber: ((value : AnyObject?)->())?) {
+        self.block = subscriber
+    }
+
+}
